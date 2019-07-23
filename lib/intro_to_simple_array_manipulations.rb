6fe6186509a5ec650f1e780adc3262a932fc2f0b
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 02:29:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,20 @@
+def using_push(array, string)
+  array = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  array.push("violet")
+end
+
+def using_unshift(array, string)
+  array = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  array.unshift("Staten Island")
+end
+
+def using_pop(array)
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  array.pop
+end
+
+def pop_with_args(array)
+  array = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  array.pop(2)
+end
+
