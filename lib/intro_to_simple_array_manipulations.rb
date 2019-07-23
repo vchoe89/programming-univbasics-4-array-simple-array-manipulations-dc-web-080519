1c11ba536749aba56e8b721b4fe83de23b9d1fea
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 02:40:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -36,6 +36,6 @@ end
 
 def using_insert(array, element)
   array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-  array.insert(4) = element
+  array.insert(element) = 4
 end
   
