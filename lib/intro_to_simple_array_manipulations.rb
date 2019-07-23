5fd99f6b253432dff672bcbc27982e1cdc0e18d8
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 02:53:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,12 +15,10 @@ def pop_with_args(array)
 end
 
 def using_shift(array)
-  array = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
   array.shift
 end
 
 def shift_with_args(array)
-  array = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
   array.shift(2)
 end
 
