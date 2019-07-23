fa343c913ce6daafe859683edf710636b379a51c
Victor Jian Choe <vchoe89@gmail.com>
2019-07-23 02:53:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,28 +23,22 @@ def shift_with_args(array)
 end
 
 def using_concat(array1, array2)
-  array1 = ["raindrops on roses", "whiskers on kittens"]
-  array2 = ["sports cars", "flatiron school"]
   array1.concat(array2)
 end
 
 def using_insert(array, element)
-  array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
   array.insert(4, element)
 end
   
 def using_uniq(array)
-  array = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
   array.uniq
 end
 
 def using_flatten(array)
-  array = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
   array.flatten
 end
   
 def using_delete(array, string)
-  array = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   array.delete
 end
   
